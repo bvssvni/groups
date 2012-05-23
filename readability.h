74d34c0d1d12ef95a723cd32aa477ef4db3679a8
Sven Nilsen <bvssvni@gmail.com>
2012-05-23 18:15:57 +0200

    Add macro for iterating through bitstreams

--- readability.h
+++ readability.h
@@ -39,3 +39,58 @@
 #define false 0
 #define string char const*
 #define byte unsigned char
+
+//
+//		FOREACH MACROES
+//
+//		These need to be copied to your source file in order to work.
+//		They simplify the task of dealing with nested bitstreams.
+//
+//		foreach (bitstream) {
+//			if (_pos(bitstream) > 200)
+//				_break(bitstream);
+//      } end_foreach
+//
+//		// Iterating reverse:
+//		foreach_reverse (bitstream) {
+//		} end_foreach
+//
+//		// Nested loops:
+//		foreach (a) {
+//			foreach (b) {
+//				_break(a); // Jumps outside the loop.
+//			} end_foreach
+//		} end_foreach
+//
+//		To access the index of the array which the bitstream
+//		is referring to, use
+//
+//		int i = _pos(prop);
+//
+//		Tip: Declaring variables before the loop will increase performance.
+//
+
+/*******************************COPY********************************
+ 
+#define foreach_reverse(a) \
+int __start##a, __end##a, __i##a, __j##a; \
+for (__i##a = a->length-2; __i##a >= 0; __i##a -= 2) { \
+__start##a = a->pointer[__i##a]; \
+__end##a = a->pointer[__i##a+1]; \
+for (__j##a = __end##prop-1; __j##a >= __start##a; __j##a--) {
+
+#define foreach(a) \
+int __len##a = a->length-1; \
+int __start##a, __end##a, __i##a, __j##a; \
+for (__i##a = 0; __i##a < __len##a; __i##a += 2) { \
+__start##a = a->pointer[__i##a]; \
+__end##a = a->pointer[__i##a+1]; \
+for (__j##a = __start##prop; __j##a < __end##a; __j##a++) {
+
+#define end_foreach }}__BREAK_BITSTREAM_prop:;
+
+#define _break(a)     goto __BREAK_BITSTREAM_##a
+
+#define _pos(a)    __j##a
+
+*******************************************************************/
